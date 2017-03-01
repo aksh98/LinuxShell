19e1c6323c093488822ca0c4579e38b045e91487
aksh98 <akarsha15010@iiitd.ac.in>
2017-03-01 21:19:10 +0530

    run file

--- run.c
+++ run.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main (){
-	//system ("gnome-terminal -e \"/home/akarsha/Desktop/SEM4/Shell/net\"");	
-system ("xterm -e \"/home/akarsha/Desktop/SEM4/Shell/2015010_shell_1\"");
+int main ()
+{
+		
+	system ("xterm -e \"/home/akarsha/Desktop/SEM4/Shell/2015010_shell_1\"");
 
-//	char* arr[] = {"-e",""};
 	return 0;
 
 }

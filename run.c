306cf80dda45b1968af0f1a6af02a1120a6c4f2e
aksh98 <akarsha15010@iiitd.ac.in>
2017-03-01 21:18:22 +0530

    run file

---
+++ run.c
@@ -0,0 +1,14 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+	//system ("gnome-terminal -e \"/home/akarsha/Desktop/SEM4/Shell/net\"");	
+system ("xterm -e \"/home/akarsha/Desktop/SEM4/Shell/2015010_shell_1\"");
+
+//	char* arr[] = {"-e",""};
+	return 0;
+
+}
